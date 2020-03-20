f8afedd9c78eec3103f442d7b9b3a016da9716f4
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-03-20 20:04:15 +0530

    MODIFIED:Doublet combination in dictionary and finded percentage of head and tail

--- flipCoinProblemStart/flipCoin.sh
+++ flipCoinProblemStart/flipCoin.sh
@@ -1,41 +1,67 @@
 #!/bin/bash -x
 #DISPLAY THE WELCOME TO FLIPCOIN STATEMENT
 echo "Welcome to flipcoin simuator"
-#GENERATING RANDOM VALUE
-result=$((RANDOM%2))
-isHead=1
-#CONDITION IS TRUE THEN HEAD OTHERWISE TAIL
-if(( $isHead == $result))
-then
-	echo "Head"
-else
-	echo "Tail"
-fi
-#CONSTANT VARIABLE
-count_H=0
-count_T=0
+#DECLARE VARIABLE
+count1=0
+count2=0
+count3=0
+count4=0
 read -p "enter a value:" user
-declare -A FlipCoin	
-#USING LOOP TO FLIP COIN MULTIPLE TIME
+declare -A FlipCoin
+FlipCoin=([H]=$count1 [T]=$count) 
+#USING LOOP FOR FLIP COIN MULTIPLE TIME
 for((iter=0;iter<$user;iter++))
 do
-	#CHECKRANDOM HEAD OR TAIL
 	checkFlipCoin=$((RANDOM%2))
 	case $checkFlipCoin in
 		0)
-			FlipCoin[$iter]="H"
-			((Count_H++))
+			FlipCoin[H]=$((${FlipCoin[H]}+1))
 			;;
 		1)
-			FlipCoin[$iter]="T"
-			((Count_T++))
+			FlipCoin[T]=$((${FlipCoin[T]}+1))
 			;;
 	esac
 done
-#PRINTING KEY AND VALUE OF COIN IN A DICTIONARY
-echo "generate singlet head and tail" ${FlipCoin[@]}
-#PRINTING PERSENTAGE OF FLIPING COIN 
-PercentageofHead=`echo "scale=2 ; ($Count_H*100)/$user" | bc -l`
+for key in "${!FlipCoin[@]}"
+do
+	echo "${key}:${FlipCoin[$key]}"
+done 
+PercentageofHead=`echo "scale=2 ;(${FlipCoin[H]}*100)/$user" | bc -l`
 echo "Percentage of Head :" $PercentageofHead
-PercentageofTail=`echo "scale=2 ; ($Count_T*100)/$user" | bc -l`
+PercentageofTail=`echo "scale=2 ;(${FlipCoin[T]}*100)/$user" | bc -l`
 echo "Percentage of Tail :" $PercentageofTail 
+#STORE THE DOUBLET OF FLIPCOIN COMBINATION
+read -p "Enter the value" user 
+FlipCoin=([HH]=$count1 [HT]=$count2 [TT]=$count3 [TH]=$count4)
+for((iter=0;iter<$user;iter++))
+do
+	checkFlipCoin=$((RANDOM%4))
+
+	case $checkFlipCoin in	
+		0)
+			FlipCoin[HH]=$((${FlipCoin[HH]}+1))
+			;;
+		1)
+			FlipCoin[TT]=$((${FlipCoin[TT]}+1))
+			;;
+		2)
+			FlipCoin[TH]=$((${FlipCoin[TH]}+1))
+			;;
+		3)
+			FlipCoin[HT]=$((${FlipCoin[HT]}+1))
+			;;
+	esac
+done
+for key in "${!FlipCoin[@]}"
+do
+   echo "${key}:${FlipCoin[$key]}"
+done 
+#PRINTING PERSENTAGE OF DOUBLET FLIPING COIN
+HHPercentage=`echo "scale=2 ;(${FlipCoin[HH]}*100)/($user)" | bc -l`
+echo "Percentage of HH :" $HHPercentage
+TTPercentage=`echo "scale=2 ;(${FlipCoin[TT]}*100)/($user)" | bc -l`
+echo "Percentage of TT:" $TTPercentage
+THPercentage=`echo "scale=2 ;(${FlipCoin[TH]}*100)/($user)" | bc -l`
+echo "Percentage of TH:" $THPercentage
+HTPercentage=`echo "scale=2 ;(${FlipCoin[HT]}*100)/($user)" | bc -l`
+echo "Percentage of HT:" $HTPercentage
