bb380c42be515141ab520c17322e60d8d08ec67d
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-03-20 10:38:10 +0530

    MODIFIED:Displayed the head and tail

--- flipCoinProblemStart/flipCoin.sh
+++ flipCoinProblemStart/flipCoin.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 #DISPLAY THE WELCOME TO FLIPCOIN STATEMENT
 echo "Welcome to flipcoin simuator"
+#GENERATING RANDOM VALUE
+result=$((RANDOM%2))
+isHead=1
+#CONDITION IS TRUE THEN HEAD OTHERWISE TAIL
+if(( $isHead == $result))
+then
+	echo "Head"
+else
+	echo "Tail"
+fi
